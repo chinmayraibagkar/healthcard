3b9bcf4f10c8d7245355f0eccc2f1c2b465888bc
Chinmay Raibagkar <139886778+chinmayraibagkar@users.noreply.github.com>
2024-12-16 15:59:11 +0530

    Update hc.py

--- hc.py
+++ hc.py
@@ -176,6 +176,7 @@ if st.session_state.fetch_data:
             st.session_state.campaign_level_weighted_avg_quality_score["Weighted avg Quality Score"] = st.session_state.campaign_level_weighted_avg_quality_score["weights"] / st.session_state.campaign_level_weighted_avg_quality_score["Impressions"]
             st.session_state.campaign_level_weighted_avg_quality_score = st.session_state.campaign_level_weighted_avg_quality_score[["Campaign Name", "Weighted avg Quality Score"]]
             st.session_state.campaign_level_weighted_avg_quality_score = st.session_state.campaign_level_weighted_avg_quality_score.sort_values(by="Weighted avg Quality Score", ascending=True)
+            st.session_state.campaign_level_weighted_avg_quality_score["Weighted avg Quality Score"].round(2)
             st.session_state.campaign_level_weighted_avg_quality_score.reset_index(drop=True, inplace=True)
             st.markdown(''':blue-background[**Weighted Average Quality Score for Campaigns**]''')
             st.dataframe(st.session_state.campaign_level_weighted_avg_quality_score) 
